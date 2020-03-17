33b3481c6e5f9753a02125688c8bdea11018d7bf
Xander de Jesus <54005135+redonioncurry@users.noreply.github.com>
2020-03-16 18:05:08 -0700

    Create SudokuSolver.java

---
+++ Sudoku/src/SudokuSolver.java
@@ -0,0 +1,17 @@
+
+public class SudokuSolver {
+
+  public static void main(String[] args) {
+
+		Puzzle puz = new Puzzle();
+		int[][] board = puz.generate();
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				System.out.print(board[i][j]);
+
+			}
+		}
+
+	}
+}
