0908a998c4b5b42d163a7d7431b128d1c707d43e
Xander de Jesus <ochnine9@gmail.com>
2020-03-21 23:26:11 -0700

    Understand recursion now. Hopefully

--- Recursion/src/MazeStarterCode.java
+++ Recursion/src/MazeStarterCode.java
@@ -36,15 +36,42 @@ public class MazeStarterCode
         // Mark Start square
         maze[1][0] = 'S';
         printMaze(maze);
-        
-        // *** Your recursive method call goes here ***
-        
+        doMaze(maze, 1, 0); 
         printMaze(maze);
     }
     
-    // Your recursive method goes here
+    public static boolean doMaze(char[][] maze, int row, int col)
+	{
+		if(row == maze.length - 2 && col == maze[0].length - 1)
+			return true;
+		if((row == 1 && col == 0) || maze[row][col] == ' ')
+		{
+			maze[row][col] = (row == 0 && col == 0) ? 'S' : '*';
+			if(right(maze, row, col))
+				return true;
+			if(down(maze, row, col))
+				return true;
+			if(left(maze, row, col))
+				return true;
+			if(up(maze, row, col))
+				return true;
+			maze[row][col] = ' ';
+			return false;
+		}
+		return false;
+	}
+	public static boolean right(char[][] maze, int row, int col)
+	{return col == maze[0].length - 1 ? false : doMaze(maze, row, col + 1);}	
     
-    private static void printMaze(char[][] maze)
+	public static boolean down(char[][] maze, int row, int col)
+	{return row == maze.length - 2 ? false : doMaze(maze, row + 1, col);}
+	
+	public static boolean left(char[][] maze, int row, int col)
+	{return col == 0 ? false : doMaze(maze, row, col - 1);}
+	
+	public static boolean up(char[][] maze, int row, int col)
+	{return row == 1 ? false : doMaze(maze, row - 1, col);}
+	private static void printMaze(char[][] maze)
     {
         for (int row = 0; row < maze.length; row++)
         {
